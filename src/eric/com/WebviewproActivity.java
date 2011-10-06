feceb59b38bddcf2a8bebc659e404a5a2546d999
nbtw23 <eric.chou@winaccord.com>
2011-10-06 12:05:54 +0800

    first commit

---
+++ src/eric/com/WebviewproActivity.java
@@ -0,0 +1,20 @@
+package eric.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebviewproActivity extends Activity {
+	WebView mWebView;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("file:///android_asset/index.htm");
+        }
+    
+}
